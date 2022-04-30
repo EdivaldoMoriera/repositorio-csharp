1386744f4fd211058ba6008ed605013f7d7a39a3
Edivaldo Moreira <37005158+EdivaldoMoriera@users.noreply.github.com>
2022-04-30 07:24:14 -0300

    Repositorio de c#

---
+++ Aluno/Aluno/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more 
+for(var i =0; i<=2; i++){
+Console.WriteLine("Infome sue nome");
+var nome = Console.ReadLine();
+Console.WriteLine("Infome sua primeira nota");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Infome sua segunda nota nota");
+int n2 = Convert.ToInt32(Console.ReadLine());
+float media;
+media = (n1 + n2) / 2f;
+if (media<5)
+{
+    Console.WriteLine("Aluno " + nome + " Reprovado " + media);
+}
+else
+{
+    Console.WriteLine("Seu Nome é " + nome + " Sua idade é " + media);
+}
+}
+
+
+
+Console.ReadKey();
---
+++ idade/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Informe seu nome");
+ var nome = Console.ReadLine();
+Console.WriteLine("Informe sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Seu nome é nome: " + nome + "Sua idade é " +idade);
+Console.ReadKey();
